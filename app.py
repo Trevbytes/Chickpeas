e6561a72e8d2b3d0b81875ba66eda159bac3a2c6
Trevbytes <trevor.j.adams@outlook.com>
2020-06-03 13:08:00 +0000

    Debugging heroku connection

--- app.py
+++ app.py
@@ -9,15 +9,11 @@ if path.exists("env.py"):
 
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = os.environ.get('MONGODB_NAME')
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-
-mongo = PyMongo(app)
 
 
 @app.route('/')
 def home():
-    return render_template(url_for('static', filename='landing_page.html'))
+    return render_template('landing_page.html')
 
 
 if __name__ == '__main__':
