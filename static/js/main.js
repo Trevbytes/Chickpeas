a86512f554be5822d1a8d81b26818f0a716de89e
Trevbytes <trevor.j.adams@outlook.com>
2020-06-05 14:28:27 +0000

    Working on landing page and add recipe pages

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
   M.AutoInit();
+  document.getElementById("defaultOpen").click();
+  $('select').formSelect();
 });
 
 function openCity(cityName, elmnt, color) {
@@ -50,3 +52,32 @@ function filterFunction() {
   }
   console.log("filter working");
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
+
