d941bcff09dbbbf9026d971f51e97c61ce247f05
Trevbytes <trevor.j.adams@outlook.com>
2020-06-11 10:19:33 +0000

    Styled add_recipe, revised colors throughout the site

---
+++ static/js/landing_page.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  M.AutoInit();
+});
+
+// Thumbnail image controls
+function currentSlide(n) {
+  clearTimeout(slidetime);
+  showSlides((slideIndex = n));
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  slidetime = setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
