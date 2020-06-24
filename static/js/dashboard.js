3febda4181a0de606b294cd70cd4b360f0886418
Trevbytes <trevor.j.adams@outlook.com>
2020-06-24 16:28:49 +0000

    Cleaned dashboard code, moved js to external file

---
+++ static/js/dashboard.js
@@ -0,0 +1,53 @@
+  function confirmDelete() {
+    if (confirm("Do you want to delete this recipe?") == false) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // Get the elements with class="column"
+  var elements = document.getElementsByClassName("column");
+
+  // Declare a loop variable
+  var i;
+
+  // List View
+  function listView() {
+    for (i = 0; i < elements.length; i++) {
+      elements[i].style.width = "100%";
+    }
+  }
+
+  // Grid View
+  function gridView() {
+    for (i = 0; i < elements.length; i++) {
+      elements[i].style.width = "50%";
+    }
+  }
+
+  $('[data-toggle="ajaxModal"]').on("click", function (e) {
+    $("#ajaxModal").remove();
+    e.preventDefault();
+    var $this = $(this),
+      $remote = $this.data("remote") || $this.attr("href"),
+      $modal = $(
+        '<div class="modal" id="ajaxModal"><div class="modal-body"></div></div>'
+      );
+    $("body").append($modal);
+    $modal.modal({ backdrop: "static", keyboard: false });
+    $modal.load($remote);
+  });
+
+  $('[data-toggle="addRecipeModal"]').on("click", function (e) {
+    $("#addRecipeModal").remove();
+    e.preventDefault();
+    var $this = $(this),
+      $remote = $this.data("remote") || $this.attr("href"),
+      $modal = $(
+        '<div class="modal" id="addRecipeModal"><div class="modal-body"></div></div>'
+      );
+    $("body").append($modal);
+    $modal.modal({ backdrop: "static", keyboard: false });
+    $modal.load($remote);
+  });
