fb8d6871cb197f46f7e66eb0773834b5a07fb9d3
Trevbytes <trevor.j.adams@outlook.com>
2020-07-21 14:31:55 +0000

    Cleaned and commented view_recipes.js

--- static/js/view_recipe.js
+++ static/js/view_recipe.js
@@ -1,37 +1,40 @@
-  function confirmDelete() {
-    if (confirm("Do you want to delete this recipe?") == false) {
-      return false;
-    } else {
-      return true;
-    }
+//Confirms deletion
+function confirmDelete() {
+  if (confirm("Are you sure you want to delete this recipe?") == false) {
+    return false;
+  } else {
+    return true;
   }
+}
 
-    $('[data-toggle="ajaxModal"]').on("click", function (e) {
-    $("#ajaxModal").remove();
-    e.preventDefault();
-    var $this = $(this),
-      $remote = $this.data("remote") || $this.attr("href"),
-      $modal = $(
-        '<div class="modal" id="ajaxModal"><div class="modal-body"></div></div>'
-      );
-    $("body").append($modal);
-    $modal.modal({ backdrop: "static", keyboard: false });
-    $modal.load($remote);
-  });
+//Ajax Modals
+$('[data-toggle="ajaxModal"]').on("click", function (e) {
+  $("#ajaxModal").remove();
+  e.preventDefault();
+  var $this = $(this),
+    $remote = $this.data("remote") || $this.attr("href"),
+    $modal = $(
+      '<div class="modal" id="ajaxModal"><div class="modal-body"></div></div>'
+    );
+  $("body").append($modal);
+  $modal.modal({ backdrop: "static", keyboard: false });
+  $modal.load($remote);
+});
 
-    $('[data-toggle="ajaxModalIngredient"]').on("click", function (e) {
-    $("#ajaxModalIngredient").remove();
-    e.preventDefault();
-    var $this = $(this),
-      $remote = $this.data("remote") || $this.attr("href"),
-      $modal = $(
-        '<div class="modal" id="ajaxModalIngredient"><div class="modal-body"></div></div>'
-      );
-    $("body").append($modal);
-    $modal.modal({ backdrop: "static", keyboard: false });
-    $modal.load($remote);
-  });
+$('[data-toggle="ajaxModalIngredient"]').on("click", function (e) {
+  $("#ajaxModalIngredient").remove();
+  e.preventDefault();
+  var $this = $(this),
+    $remote = $this.data("remote") || $this.attr("href"),
+    $modal = $(
+      '<div class="modal" id="ajaxModalIngredient"><div class="modal-body"></div></div>'
+    );
+  $("body").append($modal);
+  $modal.modal({ backdrop: "static", keyboard: false });
+  $modal.load($remote);
+});
 
+//Back button
 function goBack() {
   window.history.back();
 }
