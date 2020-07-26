cb83e43025274f21fe3e8f7ee829712a1965ad1c
Trevbytes <trevor.j.adams@outlook.com>
2020-07-26 15:45:53 +0000

    Turned debug off to push a beta test to heroku

--- app.py
+++ app.py
@@ -348,4 +348,4 @@ def sub_ingredient_search(ingredient):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
